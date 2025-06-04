c500c693981e376950d7109fbfbe8004c686085d
Juan Montilla <juan.montilla@southworks.com>
2025-06-04 10:20:50 -0300

    remove can make payment, remove unused store item

--- js/store-handler.js
+++ js/store-handler.js
@@ -3,7 +3,7 @@ import digitalGoodsService from './digital-goods.js';
 // Microsoft Store item IDs
 const STORE_ITEMS = {
   PREMIUM_FEATURES: 'super_acceso_vip',
-  REMOVE_ADS: 'remove_ads',
+  //REMOVE_ADS: 'remove_ads',
 };
 
 // Initialize the digital goods service
